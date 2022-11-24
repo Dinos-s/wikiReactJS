156584df45bf8baafc8bf659e3bf233024526934
Gabriel <gabriel02ramos@gmail.com>
2022-11-24 09:57:54 -0300

    estilizando a aba principal

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import githubLog  from "../images/githubLog.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={githubLog} alt='logo do github' width={72}/>
+    </div>
+  );
+}
+
+export default App;
